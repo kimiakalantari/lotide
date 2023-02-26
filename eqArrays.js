da57e75e2c45415f6bbec804c50662ac8910c63c
kimiakalantari <kimiakalantari97@gmail.com>
2023-02-26 14:08:16 -0500

    modified to make it better

--- eqArrays.js
+++ eqArrays.js
@@ -8,6 +8,7 @@ const assertEqual = function(actual, expected) {
 };
 
 const eqArrays = function(arr1, arr2){
+  /*
   if (arr1.length === arr2.length){
     if (!arr1.length){
       return true
@@ -20,6 +21,18 @@ const eqArrays = function(arr1, arr2){
       return true
     }
   }
+  */
+
+  let answer = true
+  if (arr1.length !== arr2.length) {
+    answer = false 
+  } 
+  for (let i = 0; i<arr1.length ; i++){
+    if (arr1[i] !== arr2[i]){
+      answer = false
+    }
+  }
+  return answer
 }
 
 // TEST CODE
