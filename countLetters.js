0dc895898ea8e615f46ba5c5436d879e8b582d2c
kimiakalantari <kimiakalantari97@gmail.com>
2023-02-27 21:51:03 -0500

    completed Implement countLetters in Lotide

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const countLetters = function(word){
+  let result = {};
+  const wordNoSpace = word.replaceAll(' ', '');
+
+  for (let letter of wordNoSpace){
+    if(result[letter]){
+      result[letter] += 1;
+    } else {
+      result[letter] = 1
+    }
+  }
+  return result;
+}
+
+const test = 'lighthouse in the house';
+console.log(countLetters(test));
